fc43db9f90680f069f48fe4cad5e9a3a1edbd437
Kareem Gan <kareemgan@gmail.com>
2017-05-10 15:34:55 -0500

    Initial commit

---
+++ tg.go
@@ -0,0 +1,80 @@
+// Package tg is a token generator
+package tg
+
+import (
+	"errors"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// None ...
+const None = 0
+
+// Lower ...
+const Lower = 1 << 0
+
+// Upper ...
+const Upper = 1 << 1
+
+// Digit ...
+const Digit = 1 << 2
+
+// Punct ...
+const Punct = 1 << 3
+
+// LowerUpper ...
+const LowerUpper = Lower | Upper
+
+// LowerDigit ...
+const LowerDigit = Lower | Digit
+
+// UpperDigit ...
+const UpperDigit = Upper | Digit
+
+// LowerUpperDigit ...
+const LowerUpperDigit = LowerUpper | Digit
+
+// All ...
+const All = LowerUpperDigit | Punct
+
+const lower = "abcdefghijklmnopqrstuvwxyz"
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const digit = "0123456789"
+const punct = "~!@#$%^&*()_+-="
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+// RandGen ...
+func RandGen(size int, set int, include string, exclude string) (string, error) {
+	all := include
+	if set&Lower > 0 {
+		all += lower
+	}
+	if set&Upper > 0 {
+		all += upper
+	}
+	if set&Digit > 0 {
+		all += digit
+	}
+	if set&Punct > 0 {
+		all += punct
+	}
+
+	lenAll := len(all)
+	if len(exclude) >= lenAll {
+		return "", errors.New("Too much to exclude.")
+	}
+	buf := make([]byte, size)
+	for i := 0; i < size; i++ {
+		b := all[rand.Intn(lenAll)]
+		if strings.Contains(exclude, string(b)) {
+			i--
+			continue
+		}
+		buf[i] = b
+	}
+	return string(buf), nil
+}
